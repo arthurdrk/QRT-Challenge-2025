ff72cc18664fd72aa0cdbd301ac8ca1ba6866155
Arthur De Rouck <arthur.de-rouck@student-cs.fr>
2025-10-01 10:11:08 +0200

    surhive

---
+++ survhive/__init__.py
@@ -0,0 +1,17 @@
+# ruff: noqa: F403
+"""
+SurvHive is a convenient, opinionated wrapper around some survival models, 
+with special emphasis on those based on deep neural networks.
+"""
+
+from .metrics import *
+from .adapter import *
+from .sksurv_adapters import *
+from .pycox_adapters import *
+from .survtrace_adapters import *
+from .lassonet_adapters import *
+from .util import *
+from .optimization import *
+from .datasets import *
+
+__version__ = "1.0.0"
